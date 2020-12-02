aea5eb05bedb5ff551dd21d0e7a60c04caec0f8d
billsegall <bill@segall.net>
2020-12-02 16:32:23 +1000

    Some basic go call and callback metrics

---
+++ cgo/callback-performance/go/perf_test.go
@@ -0,0 +1,44 @@
+package callback
+
+import (
+	"testing"
+)
+
+var numcalls int
+
+func callback(recvme int) {
+	numcalls++
+}
+
+func callf(sendme int, count int, threaded bool) bool {
+	if count <= 0 {
+		return true
+	}
+
+	if (!threaded) {
+		for i := 0; i < count; i++ {
+			callback(sendme)
+		}
+		return true;
+	}
+	return false
+}
+
+func BenchmarkSingleCall(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		callf(0x0BADBEEF, 0, false)
+	}
+}
+
+func BenchmarkOneToOne(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		callf(0x0BADBEEF, 1, false)
+	}
+}
+
+func BenchmarkOneToMillion(b *testing.B) {
+	OneMillion := 1000 * 1000
+	for n := 0; n < b.N; n += OneMillion {
+		callf(0x0BADBEEF, OneMillion, false)
+	}
+}
